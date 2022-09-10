d9bbf8c22f7ada9eb8ed50bc9bfa8f5d28fcc428
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-09-10 18:05:27 +0200

    update

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 export default {
   onLoad () {
     this.platform = this.getPlatform()
+    console.log(this.platform)
     const titlebar = cumcord.modules.webpack.find(m => m?.default?.toString?.()?.includes('case m.PlatformTypes.WINDOWS'))
+    console.log({ titlebar })
     this.unpatch = cumcord.patcher.before('default', titlebar, (...args) => {
+      console.log({ args })
       if (args[0].type !== 'WEB') {
         this.onUnload()
         return args[0]
       }
+      console.log('patching')
       args[0].type = this.platform
       return args[0]
     })
